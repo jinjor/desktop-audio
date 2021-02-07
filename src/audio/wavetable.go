215381c027a884efc8337f5e346a8f1b1177371f
Yosuke Torii <jinjorweb@gmail.com>
2021-02-07 22:43:13 +0900

    save/load wavetable

--- src/audio/wavetable.go
+++ src/audio/wavetable.go
@@ -1,24 +1,32 @@
 package audio
 
 import (
+	"encoding/binary"
+	"fmt"
 	"math"
+	"os"
 )
 
 type wavetable struct {
 	values []float64
 }
 
-func newWaveTable(samples int, phaseToValue func(phase float64) float64) *wavetable {
-	values := make([]float64, samples)
-	for i := 0; i < samples; i++ {
-		phase := 2.0 * math.Pi / float64(samples) * float64(i)
-		values[i] = phaseToValue(phase)
-	}
+func newWavetable(cap int) *wavetable {
 	return &wavetable{
-		values: values,
+		values: make([]float64, 0, cap),
 	}
 }
-
+func (wt *wavetable) generate(samples int, phaseToValue func(phase float64) float64) error {
+	if samples > cap(wt.values) {
+		return fmt.Errorf("capacity exceeded")
+	}
+	wt.values = wt.values[0:samples]
+	for i := 0; i < samples; i++ {
+		phase := 2.0 * math.Pi / float64(samples) * float64(i)
+		wt.values[i] = phaseToValue(phase)
+	}
+	return nil
+}
 func (wt *wavetable) getAtPhase(phase float64) float64 {
 	phase = math.Mod(phase, 2.0*math.Pi)
 	length := len(wt.values)
@@ -31,45 +39,113 @@ func (wt *wavetable) getAtPhase(phase float64) float64 {
 	mod := math.Mod(phase, phasePerSample)
 	return wt.values[index]*(1-mod) + wt.values[nextIndex]*mod
 }
-
-// Generation
-
-func calcFourierSeriesAtPhase(partials int, phase float64, calcFourierPartialAtPhase func(n int, phase float64) float64) float64 {
-	value := 0.0
-	for i := 1; i <= partials; i++ {
-		value += calcFourierPartialAtPhase(i, phase)
-	}
-	return value
-}
-func makeBandLimitedTableForGivenNumberOfPartials(samples int, partials int, calcFourierPartialAtPhase func(n int, phase float64) float64) *wavetable {
-	return newWaveTable(samples, func(phase float64) float64 {
-		return calcFourierSeriesAtPhase(partials, phase, calcFourierPartialAtPhase)
+func (wt *wavetable) makeBandLimitedTableForGivenNumberOfPartials(samples int, partials int, calcFourierPartialAtPhase func(n int, phase float64) float64) {
+	wt.generate(samples, func(phase float64) float64 {
+		value := 0.0
+		for i := 1; i <= partials; i++ {
+			value += calcFourierPartialAtPhase(i, phase)
+		}
+		return value
 	})
 }
-func makeBandLimitedTableWithMaxNumbersOfPartialsAtNote(samples int, note int, calcFourierPartialAtPhase func(n int, phase float64) float64) *wavetable {
+func (wt *wavetable) makeBandLimitedTableWithMaxNumbersOfPartialsAtNote(samples int, note int, calcFourierPartialAtPhase func(n int, phase float64) float64) {
 	freq := baseFreq * math.Pow(2, float64(note-69)/12)
 	partials := int(sampleRate / 2 / freq)
-	return makeBandLimitedTableForGivenNumberOfPartials(samples, partials, calcFourierPartialAtPhase)
-}
-func makeBandLimitedTablesForAllNotes(samples int, calcFourierPartialAtPhase func(n int, phase float64) float64) []*wavetable {
-	tables := make([]*wavetable, 128)
-	for i := 0; i < 128; i++ {
-		tables[i] = makeBandLimitedTableWithMaxNumbersOfPartialsAtNote(samples, i, calcFourierPartialAtPhase)
-	}
-	return tables
-}
-func calcPartialSquareAtPhase(n int, phase float64) float64 {
-	if n%2 == 1 {
-		x := float64(n)
-		return math.Sin(x*phase) / x
-	}
-	return 0.0
-}
-func calcPartialSawAtPhase(n int, phase float64) float64 {
-	x := float64(n)
-	return math.Sin(x*phase) / x
+	wt.makeBandLimitedTableForGivenNumberOfPartials(samples, partials, calcFourierPartialAtPhase)
 }
 
-// TODO: Store somewhere and load
-// var squarebltable = makeBandLimitedTablesForAllNotes(4096, calcPartialSquareAtPhase)
-// var sawbltable = makeBandLimitedTablesForAllNotes(4096, calcPartialSawAtPhase)
+// WavetableSet ...
+type WavetableSet struct {
+	tables []*wavetable
+}
+
+// NewWavetableSet ...
+func NewWavetableSet(tableCap int, sampleCap int) *WavetableSet {
+	tables := make([]*wavetable, tableCap)
+	for i := 0; i < tableCap; i++ {
+		tables[i] = newWavetable(sampleCap)
+	}
+	return &WavetableSet{
+		tables: tables,
+	}
+}
+
+// MakeBandLimitedTablesForAllNotes ...
+func (wts *WavetableSet) MakeBandLimitedTablesForAllNotes(samples int, calcFourierPartialAtPhase func(n int, phase float64) float64) error {
+	if cap(wts.tables) < 128 {
+		return fmt.Errorf("capacity of tables exceeded")
+	}
+	wts.tables = wts.tables[0:128]
+	for i := 0; i < 128; i++ {
+		wts.tables[i].makeBandLimitedTableWithMaxNumbersOfPartialsAtNote(samples, i, calcFourierPartialAtPhase)
+	}
+	return nil
+}
+
+// IO
+//   all = { number_of_tables int32, tables []table }
+//   table = { number_of_samples int32, samples []float64 }
+
+// Save ...
+func (wts *WavetableSet) Save(path string) error {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	numTables := int32(len(wts.tables))
+	err = binary.Write(file, binary.BigEndian, numTables)
+	if err != nil {
+		return err
+	}
+	for _, wt := range wts.tables {
+		numSamples := int32(len(wt.values))
+		err = binary.Write(file, binary.BigEndian, numSamples)
+		if err != nil {
+			return err
+		}
+		for _, value := range wt.values {
+			err := binary.Write(file, binary.BigEndian, value)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// Load ...
+func (wts *WavetableSet) Load(path string) error {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	var numTables int32
+	err = binary.Read(file, binary.BigEndian, &numTables)
+	if err != nil {
+		return err
+	}
+	if int(numTables) > cap(wts.tables) {
+		return fmt.Errorf("number of tables exceeded")
+	}
+	wts.tables = wts.tables[0:numTables]
+	for _, wt := range wts.tables {
+		var numSamples int32
+		err = binary.Read(file, binary.BigEndian, &numSamples)
+		if err != nil {
+			return err
+		}
+		if int(numSamples) > cap(wt.values) {
+			return fmt.Errorf("number of samples exceeded")
+		}
+		wt.values = wt.values[0:numSamples]
+		for i := range wt.values {
+			err = binary.Read(file, binary.BigEndian, &wt.values[i])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
---
+++ src/gentables/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"math"
+
+	"github.com/jinjor/desktop-audio/src/audio"
+	"golang.org/x/sync/errgroup"
+)
+
+const numTables = 128
+const numSamples = 4096
+
+func main() {
+	flag.Parse()
+	dir := flag.Arg(0)
+	if dir == "" {
+		panic("dir is not passed")
+	}
+	log.SetFlags(log.Lshortfile)
+
+	ctx := context.Background()
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		wts := audio.NewWavetableSet(numTables, numSamples)
+		wts.MakeBandLimitedTablesForAllNotes(numSamples, calcPartialSquareAtPhase)
+		log.Println("generated square wave")
+		err := wts.Save(dir + "/square")
+		log.Println("saved square wave")
+		return err
+	})
+	g.Go(func() error {
+		wts := audio.NewWavetableSet(numTables, numSamples)
+		wts.MakeBandLimitedTablesForAllNotes(numSamples, calcPartialSawAtPhase)
+		log.Println("generated saw wave")
+		err := wts.Save(dir + "/saw")
+		log.Println("saved saw wave")
+		return err
+	})
+	err := g.Wait()
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+	log.Println("Successfully generated wavetables.")
+}
+
+func calcPartialSquareAtPhase(n int, phase float64) float64 {
+	if n%2 == 1 {
+		x := float64(n)
+		return math.Sin(x*phase) / x
+	}
+	return 0.0
+}
+func calcPartialSawAtPhase(n int, phase float64) float64 {
+	x := float64(n)
+	return math.Sin(x*phase) / x
+}
