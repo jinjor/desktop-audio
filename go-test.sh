529d343980fdf3e602a19da76825a122e8c297bd
Yosuke Torii <jinjorweb@gmail.com>
2021-03-01 05:58:43 +0900

    fix shell

--- go-test.sh
+++ go-test.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -euo pipefail
+#!/bin/bash
+set -euo pipefail
 
 export GREP_OPTIONS='--color=always'
 export AUDIO_TESTING=1
+
 go clean -testcache
 go test -v ./src/audio \
   | GREP_COLOR='1;31' grep -E '.*FAIL.*|$' \
